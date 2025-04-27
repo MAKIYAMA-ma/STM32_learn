5192663f687a4b2a613dfae032f17a2b73fe4912
DESKTOP-R4JOG3F\aaayw <aaaywr29@yahoo.co.jp>
2025-04-27 13:55:39 +0900

    change DMA setting (success to loop back) change SPI clock to 20Mbps

--- NUCLEO-U575ZIQ-learn/Core/Src/stm32u5xx_hal_msp.c
+++ NUCLEO-U575ZIQ-learn/Core/Src/stm32u5xx_hal_msp.c
@@ -145,7 +145,7 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef* hspi)
     handle_GPDMA1_Channel15.Init.Request = GPDMA1_REQUEST_SPI1_TX;
     handle_GPDMA1_Channel15.Init.BlkHWRequest = DMA_BREQ_SINGLE_BURST;
     handle_GPDMA1_Channel15.Init.Direction = DMA_MEMORY_TO_PERIPH;
-    handle_GPDMA1_Channel15.Init.SrcInc = DMA_SINC_FIXED;
+    handle_GPDMA1_Channel15.Init.SrcInc = DMA_SINC_INCREMENTED;
     handle_GPDMA1_Channel15.Init.DestInc = DMA_DINC_FIXED;
     handle_GPDMA1_Channel15.Init.SrcDataWidth = DMA_SRC_DATAWIDTH_BYTE;
     handle_GPDMA1_Channel15.Init.DestDataWidth = DMA_DEST_DATAWIDTH_BYTE;
@@ -173,7 +173,7 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef* hspi)
     handle_GPDMA1_Channel14.Init.BlkHWRequest = DMA_BREQ_SINGLE_BURST;
     handle_GPDMA1_Channel14.Init.Direction = DMA_PERIPH_TO_MEMORY;
     handle_GPDMA1_Channel14.Init.SrcInc = DMA_SINC_FIXED;
-    handle_GPDMA1_Channel14.Init.DestInc = DMA_DINC_FIXED;
+    handle_GPDMA1_Channel14.Init.DestInc = DMA_DINC_INCREMENTED;
     handle_GPDMA1_Channel14.Init.SrcDataWidth = DMA_SRC_DATAWIDTH_BYTE;
     handle_GPDMA1_Channel14.Init.DestDataWidth = DMA_DEST_DATAWIDTH_BYTE;
     handle_GPDMA1_Channel14.Init.Priority = DMA_LOW_PRIORITY_LOW_WEIGHT;
